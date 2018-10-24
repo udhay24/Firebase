b115c18fe1fd98088faf7bb6ede87f1d09bbe720
udhay24 <udhayakumar241998@gmail.com>
2018-10-24 21:19:10 +0530

    log out option

--- app/src/main/java/com/example/udhay/firebase/MainActivity.java
+++ app/src/main/java/com/example/udhay/firebase/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.udhay.firebase.Adapters.ViewPagerAdapter;
@@ -13,7 +17,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
-import java.util.List;
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
     FirebaseUser firebaseUser;
 
     //View instances
-    @BindView(R.id.fragment_view_pager)
-    ViewPager viewPager;
+    @BindView(R.id.fragment_view_pager) ViewPager viewPager;
 
-    @BindView(R.id.tab_layout)
-    TabLayout tablayout;
+    @BindView(R.id.tab_layout) TabLayout tablayout;
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     //Request code
     private static final int SIGN_IN_REQUEST_CODE = 123;
@@ -45,12 +49,10 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
+
         if(firebaseUser == null){
-            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
-            .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(),
-                    new AuthUI.IdpConfig.GoogleBuilder().build()))
-                    .setIsSmartLockEnabled(false)
-                    .build(), SIGN_IN_REQUEST_CODE);
+            signInScreen();
         }else{
             Toast.makeText(this , "Sign In Successful" , Toast.LENGTH_SHORT).show();
             setUpScreenView();
@@ -62,5 +64,39 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
         tablayout.setupWithViewPager(viewPager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_activity_menu , menu);
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.log_out:
+                firebaseAuth.signOut();
+                signInScreen();
+                Toast.makeText(this, "Successfully Signed Out", Toast.LENGTH_SHORT).show();
+                return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void signInScreen(){
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.GoogleBuilder().build()))
+                .setIsSmartLockEnabled(false)
+                .build(), SIGN_IN_REQUEST_CODE);
     }
 }
