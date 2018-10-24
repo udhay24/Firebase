e92eb07e1cf94e5647fd0818c82701cec25d7769
udhay24 <udhayakumar241998@gmail.com>
2018-10-24 20:28:59 +0530

    tab layout and view pager layout is introduced

--- app/src/main/java/com/example/udhay/firebase/MainActivity.java
+++ app/src/main/java/com/example/udhay/firebase/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.udhay.firebase;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.udhay.firebase.Adapters.ViewPagerAdapter;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,12 +15,22 @@ import com.google.firebase.auth.FirebaseUser;
 import java.util.Arrays;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     //Firebase Instances
 
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
+
+    //View instances
+    @BindView(R.id.fragment_view_pager)
+    ViewPager viewPager;
+
+    @BindView(R.id.tab_layout)
+    TabLayout tablayout;
 
     //Request code
     private static final int SIGN_IN_REQUEST_CODE = 123;
@@ -30,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
 
+        ButterKnife.bind(this);
 
         if(firebaseUser == null){
             startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
@@ -39,11 +53,14 @@ public class MainActivity extends AppCompatActivity {
                     .build(), SIGN_IN_REQUEST_CODE);
         }else{
             Toast.makeText(this , "Sign In Successful" , Toast.LENGTH_SHORT).show();
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().add(R.id.message_fragment_container , new MessageFragment()).commit();
+            setUpScreenView();
         }
+    }
 
 
+    private void setUpScreenView(){
 
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+        tablayout.setupWithViewPager(viewPager);
     }
 }
