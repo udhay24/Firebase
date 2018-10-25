61eab8c02b1b2959357500c7f2ff6f379383084a
udhay <udhayakumar241998@gmail.com>
2018-10-26 00:13:18 +0530

    upload image to firebase storage

--- app/src/main/java/com/example/udhay/firebase/ImagesFragment.java
+++ app/src/main/java/com/example/udhay/firebase/ImagesFragment.java
@@ -1,19 +1,27 @@
 package com.example.udhay.firebase;
 
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -46,7 +54,7 @@ public class ImagesFragment extends Fragment {
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
         firebaseStorage = FirebaseStorage.getInstance();
-        storageReference = firebaseStorage.getReference(firebaseUser.getUid());
+        storageReference = firebaseStorage.getReference();
     }
 
     @Override
@@ -84,6 +92,31 @@ public class ImagesFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SELECT_IMAGE_REQUEST_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                Uri uri = data.getData();
+                uploadData(uri);
+            }
+        }
+    }
 
+    private void uploadData(Uri uri){
+        final AlertDialog alertDialog = new AlertDialog.Builder(ImagesFragment.this.getContext()).create();
+
+        storageReference.child(uri.getLastPathSegment());
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(ImagesFragment.this.getContext(), "Upload Success", Toast.LENGTH_SHORT).show();
+                alertDialog.cancel();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                long dataTransferred = taskSnapshot.getBytesTransferred();
+                alertDialog. setMessage(" Data Uploaded " + dataTransferred);
+                alertDialog.show();
+            }
+        });
     }
 }
